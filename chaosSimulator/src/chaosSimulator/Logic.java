cfacfc6d55cc06979619c83d024812bc10735d2b
Matt <mattcraft13@gmail.com>
2018-11-05 15:20:22 -0600

    offda

--- chaosSimulator/src/chaosSimulator/Logic.java
+++ chaosSimulator/src/chaosSimulator/Logic.java
@@ -15,7 +15,7 @@ public class Logic {
 		double deltaY = Math.abs(armY-homeY);
 		double angle = Math.atan(deltaY/deltaX);
 		double radius = Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY,2));
-		double force = Math.pow(radius/homeCoef, 2);
+		double force = Math.pow(radius/100, 2)*homeCoef;
 		double ax = force * Math.cos(angle);
 		double ay = force * Math.sin(angle);
 		if(armX < homeX) {
@@ -32,10 +32,27 @@ public class Logic {
 		//System.out.println(radius);
 		
 		//add for magnets
-		/*for(int i = 0; i < magnets.size(); i++) {
-			sum[0] += 0;
-			sum[1] += 0;
-		}*/
+		for(int i = 0; i < magnets.size(); i++) {
+			Magnet m = magnets.get(i);
+			
+			deltaX = Math.abs(m.getXPos()-armX);
+			deltaY = Math.abs(m.getYPos()-armY);
+			angle = Math.atan(deltaY/deltaX);
+			radius = Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY,2));
+			force = Math.pow(radius/100, 1)*m.getCoef();
+			ax = force * Math.cos(angle);
+			ay = force * Math.sin(angle);
+			if(armX < homeX) {
+				sum[0] += ax;
+			} else {
+				sum[0] -= ax;
+			}
+			if(armY < homeY) {
+				sum[1] += ay;
+			} else {
+				sum[1] -= ay;
+			}
+		}
 		
 		return sum;
 	}
--- chaosSimulator/src/chaosSimulator/World.java
+++ chaosSimulator/src/chaosSimulator/World.java
@@ -7,15 +7,13 @@ public class World {
 	//class contains vars about the simulation
 
 	private double armX = 50;
-
-
 	private double armY = 50;
 	private double velX = 0;
 	private double velY = 0;
 	private double homeX = Main.screensize.width/2;
 	private double homeY = Main.screensize.height/2;
-	private double defaultCoef = 1;
-	private double homeCoef = 5;
+	private double defaultCoef = 1000;
+	private double homeCoef = 100;
 	
 	private double friction = .999;
 
@@ -55,16 +53,7 @@ public class World {
 	
 	//tick for the world
 	public void tick(int framerate) {
-		/*if(armX < homeX) {
-			velX += (double)120/framerate;
-		}else {
-			velX -= (double)120/framerate;
-		}
-		if(armY < homeY) {
-			velY += (double)120/framerate;
-		}else {
-			velY -= (double)120/framerate;
-		}*/
+		//calculate acceleration
 		double[] a = Logic.acceleration(homeCoef, armX, homeX, armY, homeY, magnets);
 		
 		velX += a[0]/framerate;
