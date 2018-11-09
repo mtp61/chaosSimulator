58f3f5346d2616cd5f1e3871f77c0aa7feefdc91
Matt <mattcraft13@gmail.com>
2018-11-09 14:28:27 -0600

    new default setup

--- chaosSimulator/src/chaosSimulator/DefaultSetups.java
+++ chaosSimulator/src/chaosSimulator/DefaultSetups.java
@@ -34,7 +34,15 @@ public class DefaultSetups {
 		
 	}
 	public static void setup2(World world) {
+		world.addMagnet(new Magnet(350,350,world.getDefaultCoef()));
+		world.addMagnet(new Magnet(450,350,world.getDefaultCoef()));
+		world.addMagnet(new Magnet(450,450,world.getDefaultCoef()));
+		world.addMagnet(new Magnet(350,450,world.getDefaultCoef()));
 		
+		world.addMagnet(new Magnet(350,400,-world.getDefaultCoef()));
+		world.addMagnet(new Magnet(450,400,-world.getDefaultCoef()));
+		world.addMagnet(new Magnet(400,350,-world.getDefaultCoef()));
+		world.addMagnet(new Magnet(400,450,-world.getDefaultCoef()));
 	}
 
 
